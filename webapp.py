ef0cab7f7d809baee2e583aa7f342f0587518645
Peristratus <testcorp24@gmail.com>
2021-03-03 05:46:59 +0000

    added the code to for training and display the data and input fields to the user

--- webapp.py
+++ webapp.py
@@ -16,7 +16,7 @@ Detect is someone has diabetes using machine learning
 """)
 #open and display Image
 
-image =Image.open('/workspace/covid19_Xray_detection/dataset/ai.png')
+image =Image.open('/workspace/covid19_Xray_detection/dataset/ai.jpg')
 st.image(image, caption='ML' , use_column_width=True)
 
 #Get the data
@@ -46,3 +46,48 @@ def get_user_input():
     BMI = st.sidebar.slider('BMI', 0.0, 67.1, 32.0)
     DPF = st.sidebar.slider('DPF', 0.078, 2.42, 0.3725)
     age = st.sidebar.slider('age', 21, 81, 29)
+
+#store a dictionary into a variable
+    user_data ={'pregnancies': pregnancies,
+               'glucose':glucose,
+               'blood_presure':blood_pressure,
+                ' skin_thickness':  skin_thickness,
+                ' insulin':  insulin,
+                'BMI':BMI,
+                'DPF':DPF,
+                'age':age,
+            }
+    #Transform the data into data frame
+    features = pd.DataFrame(user_data, index=[0])
+    return features
+
+#store the user the users input into a variable
+user_input = get_user_input()
+
+#set a sub header and display the users input
+st.subheader('User Input:')
+st.write(user_input)
+
+
+#store the user input into the variable
+user_input = get_user_input()
+
+
+#set a subheader and display the users input
+st.subheader('User Input:')
+st.write(user_input)
+
+#create and train the model
+RandomForestClassifier = RandomForestClassifier()
+RandomForestClassifier.fit(X_train,  Y_train )
+
+#show the model matrix
+st.subheader('Model Test Accuracy Score:')
+st.write( str(accuracy_score(Y_test, RandomForestClassifier.predict(X_test)) * 100) + '%')
+
+#store model prediction ina variable
+prediction = RandomForestClassifier.predict(user_input)
+
+#set a subheader display the classification
+st.subheader('clasification:')
+st.write(prediction)
