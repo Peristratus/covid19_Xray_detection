867b3a2bd58cbe89f8b2552a7eab0f94c776c494
Peristratus <testcorp24@gmail.com>
2021-03-03 04:50:24 +0000

    created the training data and slider features for UX

--- webapp.py
+++ webapp.py
@@ -31,4 +31,18 @@ st.right(df.describe())
 chart = st.bar_chart(df)
 
 #split the data into independent 'X' and dependent 'Y' variables
-X = 
+X = df.iloc[:, 0:8].values
+Y = df.iloc[:, -1].values
+#split the data set into 75% Traning and 25% testing
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=0)
+
+#get the feature impot from the user 
+def get_user_input():
+    pregnancies = st.sidebar.slider('pregnancies', 0, 17, 3)
+    glucose = st.sidebar.slider('glucose', 0, 199, 117)
+    blood_pressure = st.sidebar.slider('blood_pressure', 0, 122, 72)
+    skin_thickness = st.sidebar.slider('skin_thickness', 0, 99, 23)
+    insulin = st.sidebar.slider('insulin', 0.0, 846.0, 30.0)
+    BMI = st.sidebar.slider('BMI', 0.0, 67.1, 32.0)
+    DPF = st.sidebar.slider('DPF', 0.078, 2.42, 0.3725)
+    age = st.sidebar.slider('age', 21, 81, 29)
