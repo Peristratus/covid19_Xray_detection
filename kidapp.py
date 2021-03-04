afe0606c818377d68340bd054cbadf57861336a4
Peristratus <testcorp24@gmail.com>
2021-03-04 03:41:52 +0000

    initial commit

---
+++ kidapp.py
@@ -0,0 +1,9 @@
+import glob
+from keras.models import Sequential, load_model
+import numpy as np
+import pandas as pd
+from keras.layers import Dense
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+import matpolotlib.pyplot as plt
+import keras as k
