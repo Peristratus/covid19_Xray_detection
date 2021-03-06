cb972853923772da865d673b4bc46f469d73f56b
Peristratus <testcorp24@gmail.com>
2021-03-06 20:39:03 +0000

    code testing phase to make sure that we use app.py to navigate betweenboth pages.

--- hedapp.py
+++ hedapp.py
@@ -7,86 +7,86 @@ import streamlit as st
 
 
 
-#create a title and sub-title
-st.write("""
 
-# Heart Disease
-Detect if someone has Heart Disease using Artificial Intelligence 
+def app():
+    st.title(" AI HD Detection")
+    st.write("""
+    Detect if someone has Heart Disease using Artificial Intelligence 
 
-""")
-#open and display Image
+    """)
+    #open and display Image
 
-image =Image.open('/workspace/covid19_Xray_detection/dataset/aiheart.jpg')
-st.image(image, caption='ML' , use_column_width=True)
+    image =Image.open('/workspace/covid19_Xray_detection/dataset/aiheart.jpg')
+    st.image(image, caption='ML' , use_column_width=True)
 
-#Get the data
-df = pd.read_csv('/workspace/covid19_Xray_detection/dataset/heart_statlog_cleveland_hungary_final.csv')
-#set a subheader
-st.subheader('Data Information')
-#show the data as a table
-st.dataframe(df)
-#show statistics on the data
-st.write(df.describe())
-#show the data as a chart
-chart = st.bar_chart(df)
+    #Get the data
+    df = pd.read_csv('/workspace/covid19_Xray_detection/dataset/heart_statlog_cleveland_hungary_final.csv')
+    #set a subheader
+    st.subheader('Data Information')
+    #show the data as a table
+    st.dataframe(df)
+    #show statistics on the data
+    st.write(df.describe())
+    #show the data as a chart
+    chart = st.bar_chart(df)
 
-#split the data into independent 'X' and dependent 'Y' variables
-X = df.iloc[:, 0:11].values
-Y = df.iloc[:, 0:1].values.ravel()
-#split the data set into 75% Traning and 25% testing
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=0)
+    #split the data into independent 'X' and dependent 'Y' variables
+    X = df.iloc[:, 0:11].values
+    Y = df.iloc[:, 0:1].values.ravel()
+    #split the data set into 75% Traning and 25% testing
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=0)
 
-#get the feature impot from the user 
-def get_user_input():
-    age = st.sidebar.slider('age', 16, 90, 25)
-    sex = st.sidebar.slider('sex', 0, 1, 0)
-    chest_pain = st.sidebar.slider('chest_pain', 1, 4, 0)
-    resting_bp = st.sidebar.slider('resting_bp', 0, 200, 0)
-    cholesterol = st.sidebar.slider('cholesterol', 0,700,0 )
-    fasting_blood_sugar = st.sidebar.slider('fasting_blood_sugar', 0, 1, 0)
-    resting_ecg = st.sidebar.slider('resting_ecg', 0, 2, 0)
-    max_heart_rate = st.sidebar.slider('max_heart_rate', 71, 202, 73)
-    exercise_angina = st.sidebar.slider('exercise_angina', 0, 1, 0)
-    ST_slope = st.sidebar.slider('ST_slope', 0, 2, 0)
-    oldpeak = st.sidebar.slider('oldpeak', 0.0, 3.0, 1.5)
-     
+    #get the feature impot from the user 
+    def get_user_input():
+        age = st.sidebar.slider('age', 16, 90, 25)
+        sex = st.sidebar.slider('sex', 0, 1, 0)
+        chest_pain = st.sidebar.slider('chest_pain', 1, 4, 0)
+        resting_bp = st.sidebar.slider('resting_bp', 0, 200, 0)
+        cholesterol = st.sidebar.slider('cholesterol', 0,700,0 )
+        fasting_blood_sugar = st.sidebar.slider('fasting_blood_sugar', 0, 1, 0)
+        resting_ecg = st.sidebar.slider('resting_ecg', 0, 2, 0)
+        max_heart_rate = st.sidebar.slider('max_heart_rate', 71, 202, 73)
+        exercise_angina = st.sidebar.slider('exercise_angina', 0, 1, 0)
+        ST_slope = st.sidebar.slider('ST_slope', 0, 2, 0)
+        oldpeak = st.sidebar.slider('oldpeak', 0.0, 3.0, 1.5)
+        
 
-#store a dictionary into a variable
-    user_data ={'age': age,
-               'sex':sex,
-                'chest_pain':  chest_pain,
-                'resting_bp': resting_bp,
-                 'cholesterol': cholesterol,
-                 'fasting_blood_sugar': fasting_blood_sugar,
-                 'resting_ecg':  resting_ecg,
-                 'max_heart_rate': max_heart_rate,
-                 'exercise_angina': exercise_angina,
-                 'ST_slope': ST_slope,
-                 'oldpeak': oldpeak,
+    #store a dictionary into a variable
+        user_data ={'age': age,
+                'sex':sex,
+                    'chest_pain':  chest_pain,
+                    'resting_bp': resting_bp,
+                    'cholesterol': cholesterol,
+                    'fasting_blood_sugar': fasting_blood_sugar,
+                    'resting_ecg':  resting_ecg,
+                    'max_heart_rate': max_heart_rate,
+                    'exercise_angina': exercise_angina,
+                    'ST_slope': ST_slope,
+                    'oldpeak': oldpeak,
 
-            }
-    #Transform the data into data frame
-    features = pd.DataFrame(user_data, index=[0])
-    return features
+                }
+        #Transform the data into data frame
+        features = pd.DataFrame(user_data, index=[0])
+        return features
 
-#store the user the users input into a variable
-user_input = get_user_input()
+    #store the user the users input into a variable
+    user_input = get_user_input()
 
-#set a sub header and display the users input
-st.subheader('User Input:')
-st.write(user_input)
+    #set a sub header and display the users input
+    st.subheader('User Input:')
+    st.write(user_input)
 
-#create and train the model
-RandomForestClassifier = RandomForestClassifier()
-RandomForestClassifier.fit(X_train,  Y_train )
+    #create and train the model
+    RandomForestClassifier = RandomForestClassifier()
+    RandomForestClassifier.fit(X_train,  Y_train )
 
-#show the model matrix
-st.subheader('Model Test Accuracy Score:')
-st.write( str(accuracy_score(Y_test, RandomForestClassifier.predict(X_test)) * 100) + '%')
+    #show the model matrix
+    st.subheader('Model Test Accuracy Score:')
+    st.write( str(accuracy_score(Y_test, RandomForestClassifier.predict(X_test)) * 100) + '%')
 
-#store model prediction ina variable
-prediction = RandomForestClassifier.predict(user_input)
+    #store model prediction ina variable
+    prediction = RandomForestClassifier.predict(user_input)
 
-#set a subheader display the classification
-st.subheader('clasification:')
-st.write(prediction)
+    #set a subheader display the classification
+    st.subheader('clasification:')
+    st.write(prediction)
--- webapp.py
+++ webapp.py
@@ -4,83 +4,82 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from PIL import Image
 import streamlit as st
-import webbrowser
+from multiapp import MultiApp
 
 
+def app():
+    st.title(" AI Diabetes Detection")
+    st.write("""
 
-#create a title and sub-title
-st.write("""
+    
+    Detect if someone has diabetes using Artificial Intelligence 
 
-# Diabetes Detection
-Detect if someone has diabetes using Artificial Intelligence 
+    """)
 
-""")
-#open and display Image
+    image =Image.open('/workspace/covid19_Xray_detection/dataset/diabetes.jpg')
+    st.image(image, caption='ML' , use_column_width=True)
 
-image =Image.open('/workspace/covid19_Xray_detection/dataset/diabetes.jpg')
-st.image(image, caption='ML' , use_column_width=True)
+    #Get the data
+    df = pd.read_csv('/workspace/covid19_Xray_detection/dataset/diabetes.csv')
+    #set a subheader
+    st.subheader('Data Information')
+    #show the data as a table
+    st.dataframe(df)
+    #show statistics on the data
+    st.write(df.describe())
+    #show the data as a chart
+    chart = st.bar_chart(df)
 
-#Get the data
-df = pd.read_csv('/workspace/covid19_Xray_detection/dataset/diabetes.csv')
-#set a subheader
-st.subheader('Data Information')
-#show the data as a table
-st.dataframe(df)
-#show statistics on the data
-st.write(df.describe())
-#show the data as a chart
-chart = st.bar_chart(df)
+    #split the data into independent 'X' and dependent 'Y' variables
+    X = df.iloc[:, 0:8].values
+    Y = df.iloc[:, -1].values
+    #split the data set into 75% Traning and 25% testing
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=0)
 
-#split the data into independent 'X' and dependent 'Y' variables
-X = df.iloc[:, 0:8].values
-Y = df.iloc[:, -1].values
-#split the data set into 75% Traning and 25% testing
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=0)
+    #get the feature impot from the user 
+    def get_user_input():
+        pregnancies = st.sidebar.slider('pregnancies', 0, 17, 3)
+        glucose = st.sidebar.slider('glucose', 0, 199, 117)
+        blood_pressure = st.sidebar.slider('blood_pressure', 0, 122, 72)
+        skin_thickness = st.sidebar.slider('skin_thickness', 0, 99, 23)
+        insulin = st.sidebar.slider('insulin', 0.0, 846.0, 30.0)
+        BMI = st.sidebar.slider('BMI', 0.0, 67.1, 32.0)
+        DPF = st.sidebar.slider('DPF', 0.078, 2.42, 0.3725)
+        age = st.sidebar.slider('age', 21, 81, 29)
 
-#get the feature impot from the user 
-def get_user_input():
-    pregnancies = st.sidebar.slider('pregnancies', 0, 17, 3)
-    glucose = st.sidebar.slider('glucose', 0, 199, 117)
-    blood_pressure = st.sidebar.slider('blood_pressure', 0, 122, 72)
-    skin_thickness = st.sidebar.slider('skin_thickness', 0, 99, 23)
-    insulin = st.sidebar.slider('insulin', 0.0, 846.0, 30.0)
-    BMI = st.sidebar.slider('BMI', 0.0, 67.1, 32.0)
-    DPF = st.sidebar.slider('DPF', 0.078, 2.42, 0.3725)
-    age = st.sidebar.slider('age', 21, 81, 29)
+    #store a dictionary into a variable
+        user_data ={'pregnancies': pregnancies,
+                'glucose':glucose,
+                'blood_presure':blood_pressure,
+                    ' skin_thickness':  skin_thickness,
+                    ' insulin':  insulin,
+                    'BMI':BMI,
+                    'DPF':DPF,
+                    'age':age,
+                }
+        #Transform the data into data frame
+        features = pd.DataFrame(user_data, index=[0])
+        return features
 
-#store a dictionary into a variable
-    user_data ={'pregnancies': pregnancies,
-               'glucose':glucose,
-               'blood_presure':blood_pressure,
-                ' skin_thickness':  skin_thickness,
-                ' insulin':  insulin,
-                'BMI':BMI,
-                'DPF':DPF,
-                'age':age,
-            }
-    #Transform the data into data frame
-    features = pd.DataFrame(user_data, index=[0])
-    return features
+    #store the user the users input into a variable
+    user_input = get_user_input()
 
-#store the user the users input into a variable
-user_input = get_user_input()
+    #set a sub header and display the users input
+    st.subheader('User Input:')
+    st.write(user_input)
 
-#set a sub header and display the users input
-st.subheader('User Input:')
-st.write(user_input)
+    #create and train the model
+    RandomForestClassifier = RandomForestClassifier()
+    RandomForestClassifier.fit(X_train,  Y_train )
 
-#create and train the model
-RandomForestClassifier = RandomForestClassifier()
-RandomForestClassifier.fit(X_train,  Y_train )
+    #show the model matrix
+    st.subheader('Model Test Accuracy Score:')
+    st.write( str(accuracy_score(Y_test, RandomForestClassifier.predict(X_test)) * 100) + '%')
 
-#show the model matrix
-st.subheader('Model Test Accuracy Score:')
-st.write( str(accuracy_score(Y_test, RandomForestClassifier.predict(X_test)) * 100) + '%')
+    #store model prediction ina variable
+    prediction = RandomForestClassifier.predict(user_input)
 
-#store model prediction ina variable
-prediction = RandomForestClassifier.predict(user_input)
-
-#set a subheader display the classification
-st.subheader('clasification:')
-st.write(prediction)
+    #set a subheader display the classification
+    st.subheader('clasification:')
+    st.write(prediction)
 
