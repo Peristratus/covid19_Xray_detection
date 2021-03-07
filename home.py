6a64abde651a3ba3e5a0e9e7279999dd8687ef6f
Peristratus <testcorp24@gmail.com>
2021-03-07 21:48:13 +0000

    added some text to describe the AI dataset threshold of positive or nagative.

--- home.py
+++ home.py
@@ -3,4 +3,4 @@ import streamlit as st
 def app():
     st.title('Home')
 
-    st.write(' MediStar-Artificial Inteligence Diagnosis Application.')
+    st.write(' MediStar-Artificial Inteligence Diagnosis software: Diabetes Threshold 1=Positive / 0= Negative: Heart Disease Threshold >40 positive with 70 percent accuracy < 40 negative with 70 percent accuracy   ')
