4705626a9b927e4bc6b1ad2a0a8f06168b612aa0
Peristratus <testcorp24@gmail.com>
2021-03-15 19:07:08 +0000

    changed the text on line 13 and 14

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.markdown("""
 # MedSTAR Artificial Intelligence Diagnosis Application
 
 This application is built as an Artificial Intelligence (AI) diagnostic center, this software is  dependent on Datasets, it can be used to predict the combained datasets per patient to give a ratio or 
-true/ false indication of a patients future or current Healt status.
+true/ false indication of a patients future or current Health status.
 
 """)
 
