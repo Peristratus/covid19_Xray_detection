8e68f0de1d4413568e7473dada7e907c5a5ddc7f
Peristratus <testcorp24@gmail.com>
2021-03-15 19:03:26 +0000

    changed the text on line 13 and 14

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import brcapp
 app = MultiApp()
 
 st.markdown("""
-# MediStar Artificial Intelligence Diagnosis Application
+# MedSTAR Artificial Intelligence Diagnosis Application
 
 This application is built as an Artificial Intelligence (AI) diagnostic center, this software is  dependent on Datasets, it can be used to predict the combained datasets per patient to give a ratio or 
 true/ false indication of a patients future or current Healt status.
