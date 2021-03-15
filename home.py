d37d537a725973037c598fcbbda5786382369b94
Peristratus <testcorp24@gmail.com>
2021-03-15 19:42:50 +0000

    changed the text on line 6 to be pip compliant

--- home.py
+++ home.py
@@ -3,4 +3,9 @@ import streamlit as st
 def app():
     st.title('Home')
 
-    st.write(' MediStar-Artificial Inteligence Diagnosis software. Diabetes ratio 1=Positive / 0= Negative. Heart Disease number >40 positive with 70 percent accuracy < 40 negative with 70 percent accuracy. Breast Cancer number >13 positive with 90 percent accuracy < 13 negative with 90 percent accuracy.   ')
+    st.write(""" MediStar-Artificial Inteligence Diagnosis software. 
+      Diabetes ratio 1=Positive / 0= Negative. 
+      Heart Disease number >40 positive with 70 percent accuracy 
+      < 40 negative with 70 percent accuracy. 
+      Breast Cancer number >13 positive with 90 percent accuracy 
+      < 13 negative with 90 percent accuracy. """)
